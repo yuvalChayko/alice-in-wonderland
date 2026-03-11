4f0d16ba3724839aa3e37ee7af9e23bcd070ec5c
amitcha06 <amitchayko@gmail.com>
2026-03-11 18:27:22 +0200

    fixed router.py

--- router.py
+++ router.py
@@ -1,16 +1,22 @@
 from scapy.all import *
 
-MAC = "08:00:27:aa:71:bc"
+MAC3 = "08:00:27:aa:71:bc"
+MAC8 = "08:00:27:a2:86:70"
 
 def send_packets(packet):
     new_packet = packet
-    if IP in packet and packet.dst in Net("10.100.102.0/24"):
-        new_packet[Ether].src = MAC
+    if IP in packet and packet.dst in Net("10.100.102.0/24") and new_packet.sniffed_on == "enp0s8":
+        new_packet[Ether].src = MAC3
         new_packet.ttl = packet.ttl - 1
         sendp(new_packet, iface="enp0s3")
+    
+    elif IP in packet and packet.dst in Net("92.168.137.0/24") and new_packet.sniffed_on == "enp0s3":
+        new_packet[Ether].src = MAC8
+        new_packet.ttl = packet.ttl - 1
+        sendp(new_packet, iface="enp0s8")
 
 def main():
-    sniff(iface="enp0s8", prn=send_packets)
+    sniff(iface=["enp0s8", "enp0s3"], filter="inbound", prn=send_packets)
 
 
 if __name__ == "__main__":
