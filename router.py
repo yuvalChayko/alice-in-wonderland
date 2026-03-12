82375320aef0459d49fa48c429ed6768e6f35b38
amitcha06 <amitchayko@gmail.com>
2026-03-12 17:23:15 +0200

    changed dst mac

--- router.py
+++ router.py
@@ -2,16 +2,21 @@ from scapy.all import *
 
 MAC3 = "08:00:27:aa:71:bc"
 MAC8 = "08:00:27:a2:86:70"
+FAKE_MAC3 = "08:00:27:aa:71:ab"
+FAKE_MAC8 = "08:00:27:a2:86:80"
+
 
 def send_packets(packet):
     new_packet = packet
     if IP in packet and packet.dst in Net("10.100.102.0/24") and new_packet.sniffed_on == "enp0s8":
         new_packet[Ether].src = MAC3
+        new_packet[Ether].dst = FAKE_MAC3
         new_packet.ttl = packet.ttl - 1
         sendp(new_packet, iface="enp0s3")
     
     elif IP in packet and packet.dst in Net("92.168.137.0/24") and new_packet.sniffed_on == "enp0s3":
         new_packet[Ether].src = MAC8
+        new_packet[Ether].dst = FAKE_MAC8
         new_packet.ttl = packet.ttl - 1
         sendp(new_packet, iface="enp0s8")
 
